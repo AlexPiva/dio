0fd8f2d0ba20176ab26e29aaf57e2864e699cd61
Alex Piva <81630355+AlexPiva@users.noreply.github.com>
2023-07-18 18:38:37 -0300

    Create conversao_de_tipos.py

---
+++ Curso de python/Aula 03/conversao_de_tipos.py
@@ -0,0 +1,40 @@
+# Inteiro para Float
+preco = 10
+print(preco)
+preco = float(preco)
+print(preco)
+preco = preco / 2
+print(preco)
+print()
+
+#Float para Inteiro
+preco = 10.0
+print(preco)
+preco = int(preco)
+print(preco)
+preco = preco / 2
+print(int(preco))
+print()
+
+# Conversão por divisao
+preco = 10.0
+print(preco)
+print(preco / 2)
+print(preco // 2)
+print(100 // 2)
+print()
+
+# Numérico para String
+preco = 10.50
+idade = 52
+print(str(preco))
+print(str(idade))
+texto = f'Idade {idade} e preço = {preco}'
+print(texto)
+print()
+
+# String para Numérico
+preco = "10.50"
+print(float(preco))
+idade = '28'
+print(int(idade))
