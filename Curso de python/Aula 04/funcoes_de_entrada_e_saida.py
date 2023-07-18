734d713b711b46293a5631962ec1a510da2e43a0
Alex Piva <81630355+AlexPiva@users.noreply.github.com>
2023-07-18 18:52:05 -0300

    Update funcoes_de_entrada_e_saida.py

--- Curso de python/Aula 04/funcoes_de_entrada_e_saida.py
+++ Curso de python/Aula 04/funcoes_de_entrada_e_saida.py
@@ -1,2 +1,9 @@
 # Função Input
 nome =  input('Digite seu nome: ')
+print(nome)
+
+# Função Print
+nome, sobrenome = 'Alexandre', 'Piva'
+print(nome, sobrenome)
+print(nome, sobrenome, end='...\n')
+print(nome, sobrenome, sep='#')
