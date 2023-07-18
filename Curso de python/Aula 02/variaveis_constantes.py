2da246baf7c76b185e43836f2c0d6b66e95479d8
Alex Piva <81630355+AlexPiva@users.noreply.github.com>
2023-07-18 14:06:01 -0300

    Create variaveis_constantes.py

---
+++ Curso de python/Aula 02/variaveis_constantes.py
@@ -0,0 +1,15 @@
+nome = 'Alexandre'
+idade = 52
+print(f'Meu nome é {nome} e tenho {idade} anos')
+
+nome = 'Cíntia'
+print(f'Meu nome é {nome} e tenho {idade} anos')
+
+limite_saque_diario = 1000
+
+ESTADOS = ['RS', 'SP', 'RJ', 'MG']
+print(ESTADOS)
+ESTADOS.append('AM')
+print(ESTADOS)
+ESTADOS.sort()
+print(ESTADOS)
