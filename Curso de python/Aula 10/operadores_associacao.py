ff9ed302ec3001d29dd959712bf704a9381c1f12
Alex Piva <81630355+AlexPiva@users.noreply.github.com>
2023-07-19 17:53:35 -0300

    Create operadores_associacao.py

---
+++ Curso de python/Aula 10/operadores_associacao.py
@@ -0,0 +1,11 @@
+# verifica se um objeto está em uma sequência
+
+curso = 'Curso de Python'
+frutas = ['laranja', 'uva', 'limão']
+saques = [1500, 100]
+
+print('Python' in curso)
+
+print('maça' not in frutas)
+
+print(200 in saques)
