f14990c15789c7311cb2e00a86cb26bc454c5877
Alex Piva <81630355+AlexPiva@users.noreply.github.com>
2023-07-19 00:29:21 -0300

    Create operadores_aritmeticos.py

---
+++ Curso de python/Aula 05/operadores_aritmeticos.py
@@ -0,0 +1,38 @@
+# adição
+print(5 + 1)
+
+# subtração
+print(5 - 1)
+
+# multiplicação
+print(5 * 2)
+
+# divisão
+print(5 / 2)
+
+# divisão interira
+print(5 // 2)
+
+# módulo
+print(5 % 2)
+
+# exponenciação
+print(5 ** 2)
+
+# precedência
+print(10 - 5 * 2)
+print((10 - 5) * 2)
+print(10 ** 2 * 2)
+print(10 ** (2 * 2))
+print(10 /2 *4)
+
+
+produto_1, produto_2 = 20, 10
+
+print(f'soma {produto_1 + produto_2}')
+print(f'subtração {produto_1 - produto_2}')
+print(f'produto {produto_1 * produto_2}')
+print(f'divisão {produto_1 / produto_2}')
+print(f'divisão inteira {produto_1 // produto_2}')
+print(f'módulo {produto_1 % produto_2}')
+print(f'potencia {produto_1 ** produto_2}')
