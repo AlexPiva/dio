bdc120bbc1b2f1f83da229dd9a84c06c66828e82
Alex Piva <81630355+AlexPiva@users.noreply.github.com>
2023-07-19 00:52:04 -0300

    Create operadores_atribuicao.py

---
+++ Curso de python/Aula 07/operadores_atribuicao.py
@@ -0,0 +1,31 @@
+# atribuição simples
+saldo = 500
+print(f'saldo = {saldo}')
+
+# atribuição com adição
+saldo += 200
+print(f'saldo = {saldo}')
+
+# atribuição com subtração
+saldo -= 300
+print(f'saldo = {saldo}')
+
+# atribuição com multiplicação
+saldo *= 2
+print(f'saldo = {saldo}')
+
+# atribuição com divisão
+saldo /= 2
+print(f'saldo = {saldo}')
+
+# atribuição com divisão inteira
+saldo //= 2
+print(f'saldo = {saldo}')
+
+# atribuição com módulo
+saldo %= 180
+print(f'saldo = {saldo}')
+
+# atribuição com potência
+saldo **= 2
+print(f'saldo = {saldo}')
