f4a94a3d1f2ba74696508dfe43d309510ff27d45
Alex Piva <81630355+AlexPiva@users.noreply.github.com>
2023-07-19 17:39:31 -0300

    Create operadores_identidade.py

---
+++ Curso de python/Aula 09/operadores_identidade.py
@@ -0,0 +1,8 @@
+saldo = 1000
+limite  = 1000
+
+# verifica se ocupa a mesma região de memória
+print(saldo is limite)
+
+# verifica se NÃO ocupa a mesma região de memória
+print(saldo is not limite)
