9b543586ff211f727fe949deef435355b0c8bc9a
Alex Piva <81630355+AlexPiva@users.noreply.github.com>
2023-07-24 14:45:17 -0300

    Sets (conjuntos)

---
+++ Curso de python/01 - Estrutura de dados/Aula 22 - Explorando Conjuntos em Python/01_criando_sets.py
@@ -0,0 +1,15 @@
+lista = [1, 2, 3, 1, 3, 4]
+print(lista)
+
+print(set(lista))
+
+print(set("abacaxi"))
+
+carros = (
+    "palio",
+    "gol",
+    "celta",
+    "palio",
+)
+print(carros)
+print(set(carros))
---
+++ Curso de python/01 - Estrutura de dados/Aula 23 - Métodos da classe set/10_copy.py
@@ -0,0 +1,5 @@
+sorteio = {1, 23}
+print(sorteio)
+
+sorteio1 = sorteio.copy()
+print(sorteio1)
